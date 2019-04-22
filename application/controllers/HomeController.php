fe47818bce2f53fa9d343174f107ae66b8f0bc16
Andre Luiz da Silva <andreluiz1013@hotmail.com>
2019-04-22 01:17:38 -0300

    Configuração inicial, baixando template, criando key da aplicacao

---
+++ application/controllers/HomeController.php
@@ -0,0 +1,12 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class HomeController extends CI_Controller
+{
+
+	public function index()
+	{
+		$this->template->show('home');
+	}
+}

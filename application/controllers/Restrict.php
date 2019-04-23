ef4e6fb9f241005202ae32d14bcf76b0f8920bd7
Andre Luiz da Silva <andreluiz1013@hotmail.com>
2019-04-23 12:29:02 -0300

    Criando o model de usuários (PHP)

--- application/controllers/Restrict.php
+++ application/controllers/Restrict.php
@@ -10,6 +10,9 @@ class Restrict extends CI_Controller
 		// echo password_hash("secret", PASSWORD_DEFAULT);
 		// $2y$10$gs.qemVIg7bFjYIG72xt5efbXyRf1XQBFFRZo4ecUxYNV.lsky7Eu
 
-		$this->template->show("login");
+		// $this->template->show("login");
+
+		$this->load->model('users_model');
+		print_r($this->users_model->get_user_data("andre"));
 	}
 }
---
+++ application/models/Users_model.php
@@ -0,0 +1,27 @@
+<?php
+
+class Users_model extends CI_Model
+{
+	public function __construct()
+	{
+		parent::__construct(); // Indica que estamos fazendo contrutor da classe Users_model e não da CI_Model
+		$this->load->database(); // Conexão com a base de dados
+	}
+
+	public function get_user_data($login)
+	{
+		$this->db
+			->select("id, password, name, email")
+			->from("users")
+			->where("login", $login);
+
+		$result = $this->db->get();
+
+		if($result->num_rows() > 0) {
+			return $result->row();
+		}else {
+			return null;
+		}
+
+	}
+}

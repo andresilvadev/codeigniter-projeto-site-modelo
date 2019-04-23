c8f95db76448a5ee589b3c3f066165d7e3955bbe
Andre Luiz da Silva <andreluiz1013@hotmail.com>
2019-04-23 12:19:02 -0300

    Criando e configurando a base e tabela de usuários (MySql)

--- application/controllers/Restrict.php
+++ application/controllers/Restrict.php
@@ -7,6 +7,9 @@ class Restrict extends CI_Controller
 
 	public function index()
 	{
+		// echo password_hash("secret", PASSWORD_DEFAULT);
+		// $2y$10$gs.qemVIg7bFjYIG72xt5efbXyRf1XQBFFRZo4ecUxYNV.lsky7Eu
+
 		$this->template->show("login");
 	}
 }
